157048fb8d2eecf1ab8c1b0d9dc170e444487920
yuomatsu <07yuya08@gmail.com>
2022-04-16 15:48:20 +0900

    メインループ、仮想画面作成・実画面に描画

--- js/main.js
+++ js/main.js
@@ -1,21 +1,76 @@
+const GAME_FPS = 1000 / 60;
 const SCREEN_SIZE_W = 256;
 const SCREEN_SIZE_H = 224;
 
+// 仮想キャンバスを作成
+let vcan = document.createElement("canvas");
+let vcon = vcan.getContext("2d")
+
 let can = document.getElementById("can");
 let con = can.getContext("2d");
-// console.log(con)
-// ファミコンの画面サイズ
-can.width  = SCREEN_SIZE_W;
-can.height = SCREEN_SIZE_H;
 
-con.fillStyle = "#66AAFF";
-con.fillRect(0, 0, SCREEN_SIZE_W, SCREEN_SIZE_H);
+vcan.width = SCREEN_SIZE_W;
+vcan.height = SCREEN_SIZE_H;
+
+// ファミコンの画面サイズ
+can.width = SCREEN_SIZE_W * 2;
+can.height = SCREEN_SIZE_H * 2;
+
+// ぼやけを修正
+vcon.mozimageSmoothingEnabled = false;
+vcon.msimageSmoothingEnabled = false;
+vcon.webkitimageSmoothingEnabled = false;
+vcon.imageSmoothingEnabled = false;
+
+
+let frameCount = 0;
+let startTime;
 
 let chImg = new Image();
 chImg.src = "sprite.png";
 console.log(chImg.src)
-chImg.onload = () => {
-    // 読み込み完了後に実行
-    con.drawImage(chImg, 0, 0, 16, 32, 0, 0, 16, 32);
+
+function update() {
+
 }
 
+function draw() {
+    vcon.fillStyle = "#66AAFF";
+    vcon.fillRect(0, 0, SCREEN_SIZE_W, SCREEN_SIZE_H);
+    vcon.drawImage(chImg, 0, 0, 16, 32, 0, 0, 16, 32);
+
+    vcon.font = "24px 'Impact'";
+
+    con.drawImage(vcan, 0, 0, vcan.width, vcan.height, 0, 0, can.width, can.height);
+}
+
+// 1秒間に60回mainLoopを実行
+setInterval(mainLoop, 1000 / 60);
+
+window.onload = function () {
+    startTime = performance.now();
+    mainLoop();
+}
+
+function mainLoop() {
+    let nowTime = performance.now();
+
+    // 何回目の描画なのか算出
+    let nowFrame = (nowTime - startTime) / GAME_FPS;
+    if (nowFrame > frameCount) {
+
+        // 処理落ちした時の制御（もっといいやり方あるかも）
+        let c = 0;
+        while (nowFrame > frameCount) {
+            frameCount++;
+            // 更新処理
+            update();
+            if (++c >= 4) break;
+        }
+        // 描画処理
+        draw();
+
+    }
+    requestAnimationFrame(mainLoop);
+
+}
